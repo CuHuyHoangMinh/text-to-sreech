5cc32e11ba036f9719db9062f938cd55885bb26c
BrassM0nkeyy <mgrech97@gmail.com>
2016-12-03 14:51:17 -0500

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,14 +1,9 @@
-(function() {
-  // receives message from popup script
-  chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    if (request.opened == true) {
-      // sends response back to popup script
-      sendResponse({example: "goodbye"});
-
-      // sends response to content script
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { logUrl: true } );
-      });
-    }
+chrome.browserAction.onClicked.addListener(function (tab) {
+    //fired when the user clicks on the ext's icon
+    sendMessage();
+});
+function sendMessage() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {action: "pageToSpeech"}, function(response) {});
   });
-})();
+}
