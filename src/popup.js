0ef176361a023a156723b924d4fcfb8691acba8f
ZackNathan <zack.nathan@ucc.on.ca>
2016-12-03 16:15:18 -0500

    Prints selected text to console

--- src/popup.js
+++ src/popup.js
@@ -4,3 +4,19 @@ $(function() {
     console.log(response.example);
   });
 });
+
+function screech() {
+    chrome.tabs.executeScript( {
+        code: "window.getSelection().toString();"
+    }, function(selection) {
+        chrome.extension.getBackgroundPage().console.log(selection[0]);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('click-this');
+
+    link.addEventListener('click', function() {
+        screech();
+    });
+});
