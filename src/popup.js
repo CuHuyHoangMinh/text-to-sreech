a284dc919b4dbf992282582d24e12254ceb80ff2
ZackNathan <zack.nathan@ucc.on.ca>
2016-12-03 16:39:36 -0500

    Reads selected text

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,30 @@
 $(function() {
-  // sends message to background script
-  chrome.runtime.sendMessage({opened: true}, function(response) {
-    console.log(response.example);
-  });
+    // sends message to background script
+    chrome.runtime.sendMessage({opened: true}, function(response) {
+        console.log(response.example);
+    });
 });
 
-function screech() {
+function getSelectionText() {
     chrome.tabs.executeScript( {
         code: "window.getSelection().toString();"
     }, function(selection) {
-        chrome.extension.getBackgroundPage().console.log(selection[0]);
+        textToScreech(selection[0]);
     });
+}
+
+function textToScreech(text) {
+    chrome.extension.getBackgroundPage().console.log(text);
+
+    for (var i=0, len=text.length; i<len; i++) {
+        chrome.extension.getBackgroundPage().console.log(text[i]);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('click-this');
 
     link.addEventListener('click', function() {
-        screech();
+        getSelectionText();
     });
 });
