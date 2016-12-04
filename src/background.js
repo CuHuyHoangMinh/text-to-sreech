46198fff10d115bbccfdf18b50233e1c37de4243
ZackNathan <zack.nathan@ucc.on.ca>
2016-12-03 20:00:02 -0500

    sldkfjhlweiubf

--- src/background.js
+++ src/background.js
@@ -2,8 +2,21 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     //fired when the user clicks on the ext's icon
     sendMessage();
 });
+
 function sendMessage() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, {action: "pageToSpeech"}, function(response) {});
   });
 }
+
+var screams = [];
+
+function read_audio() {
+    for (var i = 0; i < 41; i++) {
+        var scream = new Audio();
+        scream.src = "audio/"+i.toString()+".mp3";
+        screams.push(scream);
+    }
+}
+
+read_audio();
--- src/popup.js
+++ src/popup.js
@@ -5,6 +5,9 @@ $(function() {
     });
 });
 
+var background = chrome.extension.getBackgroundPage();
+var screams = background.screams;
+
 function getSelectionText() {
     chrome.tabs.executeScript( {
         code: "window.getSelection().toString();"
@@ -14,11 +17,33 @@ function getSelectionText() {
 }
 
 function textToScreech(text) {
-    chrome.extension.getBackgroundPage().console.log(text);
+    // chrome.extension.getBackgroundPage().console.log(text);
 
-    // for (var i=0, len=text.length; i<len; i++) {
-    //     chrome.extension.getBackgroundPage().console.log(text[i]);
-    // }
+    for (var i=0; i<text.length; i++) {
+        var char = text[i];
+        char = char.toUpperCase();
+        var x = char.charCodeAt(0);
+        chrome.extension.getBackgroundPage().console.log(x);
+
+        if (x == 32) {
+            screams[0].play();
+        } else if (x <= 57 && x >= 48) {
+            screams[x-47].play();
+        } else if (x <= 90 && x >= 65) {
+            screams[x-54].play();
+        } else if (x == 33) {
+            screams[37].play();
+        } else if (x == 44 || x == 46) {
+            screams[38].play();
+        } else if (x == 63) {
+            screams[39].play();
+        } else {
+            screams[40].play();
+        }
+
+        for (var j=0; j<300000000; j++) {}
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
